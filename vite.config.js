4e4e745296bc807ccfab046af3c12c0af067d514
Saman Tofighian <samantofighian78@gamil.com>
2024-07-08 21:32:23 +0430

    up6

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/saman-snappfood/",
+  base: "/<saman-snappfood>/",
   plugins: [react()],
 });
