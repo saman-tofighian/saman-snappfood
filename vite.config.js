01d0308bb42785e258ed63764a72b17f7c4842a3
Saman Tofighian <samantofighian78@gamil.com>
2024-07-08 21:48:22 +0430

    ready to use

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/saman-snappfood/",
   plugins: [react()],
-  resolve: {
-    alias: {
-      "react-router-dom": "react-router-dom",
-      aos: "aos", // اضافه کردن alias برای aos
-    },
-  },
 });
